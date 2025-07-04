17caf1c46c4489508d7926afd9a6a2c25e0c7c5c
Vegancat <mmostafavi247@gmail.com>
2025-07-05 00:19:23 +0330

    Add initial configuration files and source structure for the LOL game shared config

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export * from "./constants/games";
+export * from "./constants/mine";
+export * from "./constants/lab";
+export * from "./constants/factory";
+export * from "./constants/general";
+export * from "./constants/energyGenerator";
+export * from "./constants/payment";
+export * from "./constants/shop";
+export * from "./constants/profile";
+
